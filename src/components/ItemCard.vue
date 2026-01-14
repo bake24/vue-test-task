8ca68b650fa2adfa77549661fad157f5b13902d4
bakdauletduisenbekov <duisenbekov07@gmail.com>
2026-01-14 12:55:18 -0500

    structured version

---
+++ src/components/ItemCard.vue
@@ -0,0 +1,34 @@
+<script setup>
+const props = defineProps({
+  label: {
+    type: String,
+    required: true
+  },
+  removable: {
+    type: Boolean,
+    default: false
+  },
+  removeDisabled: {
+    type: Boolean,
+    default: false
+  }
+})
+
+defineEmits(['remove'])
+</script>
+
+<template>
+  <div class="item-card" :class="{ 'item-card--selected': props.removable }">
+    <span class="item-label">{{ props.label }}</span>
+    <button
+      v-if="props.removable"
+      type="button"
+      class="item-remove"
+      :disabled="props.removeDisabled"
+      :aria-label="`Remove ${props.label}`"
+      @click="$emit('remove')"
+    >
+      x
+    </button>
+  </div>
+</template>
---
+++ src/components/SelectionPanel.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  variant: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
+<template>
+  <section class="panel" :class="variant ? `panel--${variant}` : null">
+    <div class="panel__header">{{ title }}</div>
+    <div class="panel__content">
+      <slot />
+    </div>
+    <div v-if="$slots.meta" class="panel__meta">
+      <slot name="meta" />
+    </div>
+  </section>
+</template>
